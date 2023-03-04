37ca270984bb730b464fcf4a2bcaf31f11f3c4a2
Gayle Hoefer <gaylehoefer@gmail.com>
2023-03-04 16:58:22 -0700

    Cover the first half of fundimentals of type script

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,8 @@
 "use strict";
-let age = 20;
+const small = 1;
+const medium = 2;
+const large = 3;
+;
+let mySize = 2;
+console.log(mySize);
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-let age: number = 20;
+// let sales: number = 123_456_789;
+// let course: string = 'TypeScript';
+// let is_published: boolean = true;
+// let level;
+
+// any type
+// function render(document: any) {
+//     console.log(document);
+// }
+
+// arrays
+// let numbers: number[] = [];
+// numbers.forEach(n => n.toExponential)
+
+// Tuple is a fix length array where each item is a fix type
+// restrict it to two values
+// let user: [number, string] =[1, 'Mosh']
+// user.push(1);
+
+// Enums
+const small = 1;
+const medium = 2;
+const large = 3;
+// Use PascalCase
+// Will start at 0
+const enum Size { Small = 1, Medium, Large};
+let mySize: Size =Size.Medium;
+console.log(mySize);
