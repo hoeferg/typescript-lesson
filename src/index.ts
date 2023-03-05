6cba9579c372d673e8e979a5b4660aea9e3c83ce
Gayle Hoefer <gaylehoefer@gmail.com>
2023-03-05 15:38:04 -0700

    Went over union types and intersection types

--- src/index.ts
+++ src/index.ts
@@ -18,19 +18,19 @@
 // user.push(1);
 
 // Enums
-    // const small = 1;
-    // const medium = 2;
-    // const large = 3;
-    // // Use PascalCase
-    // // Will start at 0
-    // const enum Size { Small = 1, Medium, Large};
-    // let mySize: Size =Size.Medium;
-    // console.log(mySize);
+// const small = 1;
+// const medium = 2;
+// const large = 3;
+// // Use PascalCase
+// // Will start at 0
+// const enum Size { Small = 1, Medium, Large};
+// let mySize: Size =Size.Medium;
+// console.log(mySize);
 
 // function
-    // define the value type for both in input and the output. 
-    // The last number in the top line of the function is the number
-    // We can make a variable optional by using a ?
+// define the value type for both in input and the output. 
+// The last number in the top line of the function is the number
+// We can make a variable optional by using a ?
 
 // function calculateTax(income: number, taxYear = 2022): number {
 //     if ((taxYear  < 2022) 
@@ -51,4 +51,52 @@
 //     retire: (date: Date) => {
 //         console.log(date);
 //     } 
+// }
+
+// type alias
+// type Employee = {
+//     readonly id: number,
+//     name: string,
+//     retire: (date: Date) => void
+// }
+
+// let employee : Employee = {  
+//     id : 1,
+//     name: ' Gayle', 
+//     retire: (date: Date) => {
+//         console.log(date);
+//     } 
+// }
+
+// Union Types
+// Give variable or function paramiter more then one type
+// function kgToLbs(weight: number | string): number {
+//     //    Narrowing
+//     if (typeof weight === 'number')
+//         return weight * 2.2;
+//     else
+//         return parseInt(weight) * 2.2;
+// }
+
+
+// kgToLbs(10);
+// kgToLbs('10kg');
+
+//  Intersection Types
+// is an object that is both a number and a string
+// let weight: number & string;
+
+// type Draggable = {
+//     drag: () => void
+// };
+
+// type Resizable = {
+//     resize: () => void
+// };
+
+// type UIWidget = Draggable & Resizable;
+
+// let textBox: UIWidget = {
+//     drag: () => {},
+//     resize: () => {},
 // }
